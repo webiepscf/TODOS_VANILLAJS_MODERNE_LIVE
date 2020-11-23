343db02bba9052e3be751fe19299dfd0cceb3d91
pascalllacroix <webiepscf@gmail.com>
2020-11-23 15:47:01 +0100

    Intégration du template du TodoList

--- src/assets/js/modules/TodoList.js
+++ src/assets/js/modules/TodoList.js
@@ -1,14 +1,20 @@
 import Todo from './Todo';
+import template from './templates/todoList';
 
 export default class TodoList {
   constructor (data) {
     this.el    = document.querySelector(data.el);
     this.todos = [];
     this.loadData(data.todos);
+    this.template = template;
+    this.render();
   }
   loadData(data) {
     for (const item of data) {
       this.todos.push(new Todo(item));
     }
+  }
+  render () {
+    this.el.innerHTML = this.template;
   }
 }
